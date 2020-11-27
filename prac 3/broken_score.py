9dcd42344a5b0a5806e6d8a95b6708038d61bdec
foooooooooooooooooooooooooootw <foooooooooooooooooooooooooootw@gmail.com>
2020-11-27 11:18:16 +0800

    updated broken_score.py & temperatures.py

--- prac 3/broken_score.py
+++ prac 3/broken_score.py
@@ -7,13 +7,18 @@ Broken program to determine score status
 
 score = float(input("Enter score: "))
 
+
+def grade_bracket():
+    if score >= 90:
+        return ("Excellent")
+    elif score >= 50:
+        return ("Passable")
+    else:
+        return ("Bad")
+
+
 if score < 0 or score > 100:
     print("Invalid score")
 
 else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    else:
-        print("Bad")
+    print(grade_bracket())
