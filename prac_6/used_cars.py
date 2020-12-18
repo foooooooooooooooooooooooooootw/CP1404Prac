6e0a3f45d653b0d16a8aadc64ec73f900ae09226
foooooooooooooooooooooooooootw <foooooooooooooooooooooooooootw@gmail.com>
2020-12-18 10:54:17 +0800

    update car.py & used_cars.py

--- prac_6/used_cars.py
+++ prac_6/used_cars.py
@@ -8,9 +8,19 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
     my_car.drive(30)
+    my_car.name = "Car"
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
+
+    limo = Car(100)
+    limo.add_fuel(20)
+    print("limo fuel =", limo.fuel)
+    limo.drive(115)
+    print("limo odo =", limo.odometer)
+    limo.name = "Limo"
+    print(limo)
+
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
