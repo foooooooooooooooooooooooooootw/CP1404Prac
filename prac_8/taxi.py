013bba75f41cb402ea92c0843a7f6cc1f079a87c
foooooooooooooooooooooooooootw <foooooooooooooooooooooooooootw@gmail.com>
2021-01-08 10:49:06 +0800

    Second deliverable modification to taxi_test.py + reformat code

--- prac_8/taxi.py
+++ prac_8/taxi.py
@@ -6,12 +6,12 @@ from prac_8.car import Car
 
 
 class Taxi(Car):
+    price_per_km = 1.23
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name = "", fuel = 0, price_per_km = 0):
+    def __init__(self, name = "", fuel = 0):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
--- prac_8/taxi_test.py
+++ prac_8/taxi_test.py
@@ -2,11 +2,9 @@ from prac_8.taxi import Taxi
 
 
 def main():
-
     Prius1 = Taxi()
     Prius1.name = "Prius 1"
     Prius1.fuel = 100
-    Prius1.price_per_km = 1.23
     Prius1.drive(40)
     print(Prius1)
     Prius1.start_fare()
@@ -15,4 +13,3 @@ def main():
 
 
 main()
-
