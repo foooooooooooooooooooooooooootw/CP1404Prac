2466ced0be80b08c2d02f094d823346db6f603de
foooooooooooooooooooooooooootw <foooooooooooooooooooooooooootw@gmail.com>
2021-02-02 16:54:40 +0800

    add greet route

--- prac 10/flaskProject/app.py
+++ prac 10/flaskProject/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello World :)</h1>'
 
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name="FTW"):
+    return "Hello {}".format(name)
+
 
 if __name__ == '__main__':
     app.run()
